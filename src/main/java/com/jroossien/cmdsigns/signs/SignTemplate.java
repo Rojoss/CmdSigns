70a49fe46a04ed051ba4db52d91ec86f714e9ab9
Jos <josroossien@hotmail.com>
2016-01-09 19:13:23 +0000

    Added the YamlConfiguration to each template. Added save method in template to save the template to config. Added setters for all properties in template and make them modify the config values too.

--- src/main/java/com/jroossien/cmdsigns/signs/SignTemplate.java
+++ src/main/java/com/jroossien/cmdsigns/signs/SignTemplate.java
@@ -1,6 +1,14 @@
 package com.jroossien.cmdsigns.signs;
 
+import com.jroossien.cmdsigns.CmdSigns;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
 public class SignTemplate {
+
+    private YamlConfiguration config;
 
     private String name;
     private boolean enabled;
@@ -8,12 +16,17 @@ public class SignTemplate {
     private String[] syntax;
     private String[] commands;
 
-    public SignTemplate(String name, String[] syntax, String[] commands, int uniqueLine, boolean enabled) {
+    public SignTemplate(YamlConfiguration config, String name, String[] syntax, String[] commands, int uniqueLine, boolean enabled) {
+        this.config = config;
         this.name = name;
         this.syntax = syntax;
         this.commands = commands;
         this.uniqueLine = uniqueLine;
         this.enabled = enabled;
+    }
+
+    public YamlConfiguration getConfig() {
+        return config;
     }
 
     public String getName() {
@@ -24,8 +37,18 @@ public class SignTemplate {
         return enabled;
     }
 
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        config.set("enabled", enabled);
+    }
+
     public int getUniqueLine() {
         return uniqueLine;
+    }
+
+    public void setUniqueLine(int uniqueLine) {
+        this.uniqueLine = uniqueLine;
+        config.set("uniqueLine", uniqueLine+1);
     }
 
     public String[] getSyntax() {
@@ -39,10 +62,30 @@ public class SignTemplate {
         return syntax[line];
     }
 
+    public void setSyntax(int line, String syntax) {
+        this.syntax[line] = syntax;
+        config.set("syntax." + (line+1), syntax);
+    }
+
     public String getCommand(CmdTrigger trigger) {
         if (trigger == null || trigger.ordinal() >= commands.length) {
             return "";
         }
         return commands[trigger.ordinal()];
+    }
+
+    public void setCommand(CmdTrigger trigger, String cmd) {
+        this.commands[trigger.ordinal()] = cmd;
+        config.set("commands." + trigger.toString().toLowerCase().replace("_", "-"), cmd);
+    }
+
+    public void save() {
+        CmdSigns.inst().getTM().setTemplate(this);
+        File file = new File(CmdSigns.inst().getTM().getTemplateDir(), getName() + ".yml");
+        try {
+            getConfig().save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
