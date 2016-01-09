c61463ae97a899e04d1e9d4cb8be00c68d375786
Jos <josroossien@hotmail.com>
2016-01-09 17:28:21 +0000

    Added names to triggers

--- src/main/java/com/jroossien/cmdsigns/signs/CmdTrigger.java
+++ src/main/java/com/jroossien/cmdsigns/signs/CmdTrigger.java
@@ -1,8 +1,18 @@
 package com.jroossien.cmdsigns.signs;
 
 public enum CmdTrigger {
-    LEFT,
-    RIGHT,
-    SHIFT_LEFT,
-    SHIFT_RIGHT;
+    LEFT("Left click"),
+    RIGHT("Right click"),
+    SHIFT_LEFT("Shift + Left click"),
+    SHIFT_RIGHT("Shift + Right click");
+
+    private String name;
+
+    CmdTrigger(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
