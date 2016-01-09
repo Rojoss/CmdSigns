c276e7ccc2986076559561eceb036838cccb87ce
Jos <josroossien@hotmail.com>
2016-01-09 17:31:19 +0000

    Added a bunch of utility methods

---
+++ src/main/java/com/jroossien/cmdsigns/util/Util.java
@@ -0,0 +1,240 @@
+package com.jroossien.cmdsigns.util;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.*;
+
+public class Util {
+
+    /**
+     * Integrate ChatColor in a string based on color codes.
+     * @param str The string to apply color to.
+     * @return formatted string
+     */
+    public static String color(String str) {
+        for (ChatColor c : ChatColor.values()) {
+            str = str.replaceAll("&" + c.getChar() + "|&" + Character.toUpperCase(c.getChar()), "§" + c.getChar());
+        }
+        return str;
+    }
+
+    /**
+     * Integrate ChatColor in a string based on color codes.
+     * @param str The string to apply color to.
+     * @return formatted string
+     */
+    public static String colorChatColors(String str) {
+        for (ChatColor c : ChatColor.values()) {
+            str = str.replaceAll("&" + c.getChar() + "|&" + Character.toUpperCase(c.getChar()), c.toString());
+        }
+        return str;
+    }
+
+    /**
+     * Remove all color and put colors as the formatting codes like &amp;1.
+     * @param str The string to remove color from.
+     * @return formatted string
+     */
+    public static String removeColor(String str) {
+        for (ChatColor c : ChatColor.values()) {
+            str = str.replace(c.toString(), "&" + c.getChar());
+        }
+        return str;
+    }
+
+    /**
+     * Strips all coloring from the specified string.
+     * @param str The string to remove color from.
+     * @return String without any colors and without any color symbols.
+     */
+    public static String stripAllColor(String str) {
+        return ChatColor.stripColor(colorChatColors(str));
+    }
+
+
+
+    public static String implode(Object[] arr, String glue, String lastGlue, int start, int end) {
+        String ret = "";
+
+        if (arr == null || arr.length <= 0)
+            return ret;
+
+        for (int i = start; i <= end && i < arr.length; i++) {
+            if (i >= end-1 || i >= arr.length-2) {
+                ret += arr[i].toString() + lastGlue;
+            } else {
+                ret += arr[i].toString() + glue;
+            }
+        }
+
+        if (ret.trim().isEmpty()) {
+            return ret;
+        }
+        return ret.substring(0, ret.length() - lastGlue.length());
+    }
+
+    public static String implode(Object[] arr, String glue, int start) {
+        return implode(arr, glue, glue, start, arr.length - 1);
+    }
+
+    public static String implode(Object[] arr, String glue, String lastGlue) {
+        return implode(arr, glue, lastGlue, 0, arr.length - 1);
+    }
+
+    public static String implode(Object[] arr, String glue) {
+        return implode(arr, glue, 0);
+    }
+
+    public static String implode(Collection<?> args, String glue) {
+        if (args.isEmpty())
+            return "";
+        return implode(args.toArray(new Object[args.size()]), glue);
+    }
+
+    public static String implode(Collection<?> args, String glue, String lastGlue) {
+        if (args.isEmpty())
+            return "";
+        return implode(args.toArray(new Object[args.size()]), glue, lastGlue);
+    }
+
+
+
+    public static Map<String, File> getFiles(File dir, final String extension) {
+        Map<String, File> names = new HashMap<String, File>();
+        if (dir == null || !dir.exists() || !dir.isDirectory()) {
+            return names;
+        }
+
+        File[] files = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith("." + extension);
+            }
+        });
+
+        for (File file : files) {
+            names.put(file.getName().substring(0, file.getName().length() - extension.length() - 1), file);
+        }
+
+        return names;
+    }
+
+
+    public static List<String> splitNewLinesList(List<String> list) {
+        if (list != null && list.size() > 0) {
+            List<String> loreList = new ArrayList<String>();
+            List<String> listClone = list;
+            for (String string : listClone) {
+                loreList.addAll(Arrays.asList(string.split("\n")));
+            }
+            return loreList;
+        }
+        return list;
+    }
+
+
+    /**
+     * Get a Color from a string.
+     * The string can be either rrr,ggg,bbb or #hexhex or without the hashtag.
+     * It can also be a name of a color preset.
+     */
+    public static Color getColor(String string) {
+        if (string.isEmpty()) {
+            return Color.WHITE;
+        }
+        if (string.contains("#")) {
+            string = string.replace("#", "");
+        }
+
+        if (string.split(",").length > 2) {
+            return getColorFromRGB(string);
+        } else if (string.matches("[0-9A-Fa-f]+")) {
+            return getColorFromHex(string);
+        } else {
+            //TODO: Return color from preset.
+            return null;
+        }
+    }
+
+    public static Color getColorFromHex(String string) {
+        int c = 0;
+        if (string.contains("#")) {
+            string = string.replace("#", "");
+        }
+        if (string.matches("[0-9A-Fa-f]+")) {
+            return Color.fromRGB(Integer.parseInt(string, 16));
+        }
+        return null;
+    }
+
+    public static Color getColorFromRGB(String string) {
+        String[] split = string.split(",");
+        if (split.length < 3) {
+            return null;
+        }
+        Integer red = Util.getInt(split[0]);
+        Integer green = Util.getInt(split[1]);
+        Integer blue = Util.getInt(split[2]);
+        if (red == null || green == null || blue == null) {
+            return null;
+        }
+        return Color.fromRGB(Math.min(Math.max(red, 0), 255), Math.min(Math.max(green, 0), 255), Math.min(Math.max(blue, 0), 255));
+    }
+
+
+    /**
+     * Convert a string like '1' to a int. Returns null if it's invalid.
+     * @param str
+     * @return int
+     */
+    public static Integer getInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+        }
+        return null;
+    }
+
+    /**
+     * Convert a string like '1' to a long. Returns null if it's invalid.
+     * @param str
+     * @return int
+     */
+    public static Long getLong(String str) {
+        try {
+            return Long.parseLong(str);
+        } catch (NumberFormatException e) {
+        }
+        return null;
+    }
+
+    /**
+     * Convert a string like '1.5' to a double. Returns null if it's invalid.
+     * @param str
+     * @return double
+     */
+    public static Double getDouble(String str) {
+        try {
+            return Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+        }
+        return null;
+    }
+
+    /**
+     * Convert a string like '1.12' to a float. Returns null if it's invalid.
+     * @param str
+     * @return float
+     */
+    public static Float getFloat(String str) {
+        if (str != null && str != "") {
+            try {
+                return Float.parseFloat(str);
+            } catch (NumberFormatException e) {
+            }
+        }
+        return null;
+    }
+}
